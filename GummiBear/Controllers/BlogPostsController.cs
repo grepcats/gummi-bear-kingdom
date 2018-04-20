db09d1b6dd51621918fec4ec233fe95f6686528c
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-20 15:23:07 -0700

    add ability to create new blog posts

--- GummiBear/Controllers/BlogPostsController.cs
+++ GummiBear/Controllers/BlogPostsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummiBear.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,24 @@ namespace GummiBear.Controllers
 {
     public class BlogPostsController : Controller
     {
-        // GET: /<controller>/
+        private GummiBearContext db = new GummiBearContext();
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogPost blogPost)
+        {
+            blogPost.PostDate = DateTime.Now;
+            db.BlogPosts.Add(blogPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
