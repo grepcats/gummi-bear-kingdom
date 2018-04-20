0db3948e3861864d88296855994057bf19285c7b
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-20 08:49:25 -0700

    add product class

---
+++ GummiBear/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBear.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
--- GummiBear/README.md
+++ GummiBear/README.md
@@ -1,1 +1,7 @@
-﻿
+﻿## Specs
+* Program should list all available products (name, cost, description)
+* Program should have a landing page from where user can go see products
+* A user should be able to click on each product and see details
+* An admin should be able to add new products
+* An admin should be able to remove individual products
+* An admin should be able to remove all products
