9f879dfaa85f80ff604d042ddae3acbe783ee71d
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-20 10:07:16 -0700

    add model to ProductsController and table to corresponding view

--- GummiBear/Controllers/ProductsController.cs
+++ GummiBear/Controllers/ProductsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GummiBear.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,12 @@ namespace GummiBear.Controllers
 {
     public class ProductsController : Controller
     {
+        private GummiBearContext db = new GummiBearContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Product> model = db.Products.ToList();
+            return View(model);
         }
     }
 }
