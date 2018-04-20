cc91ca8981549d204eec90c89307a0134b33e554
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-20 09:14:33 -0700

    add setup for database, including GummiBearContext class

---
+++ GummiBear/Models/GummiBearContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBear.Models
+{
+    public class GummiBearContext : DbContext
+    {
+        public GummiBearContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(@"Server=localhost;Port=8889;database=GummiBear;uid=root;pwd=root;");
+        }
+
+        public GummiBearContext(DbContextOptions<GummiBearContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- GummiBear/Startup.cs
+++ GummiBear/Startup.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using GummiBear.Models;
 
 namespace GummiBear
 {
@@ -27,6 +31,7 @@ namespace GummiBear
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFramework().AddDbContext<GummiBearContext>(options => options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
             // Add framework services.
             services.AddMvc();
         }
@@ -54,6 +59,11 @@ namespace GummiBear
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
