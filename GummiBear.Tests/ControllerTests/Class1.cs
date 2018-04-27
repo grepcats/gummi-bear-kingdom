f6ea79b81cc6c0baf7315c5e5757bf4042a1ef2c
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-27 09:07:50 -0700

    add HomeController test for returning view

---
+++ GummiBear.Tests/ControllerTests/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GummiBear.Controllers;
+//using GummiBear.Models.Repositories;
+using GummiBear.Models;
+using Microsoft.AspNetCore.Mvc;
+//using Moq;
+using System.Linq;
+
+namespace GummiBear.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void HomeController_ReturnView_View()
+        {
+            //arrange
+            HomeController controller = new HomeController();
+
+            //act
+            var indexView = new HomeController().Index();
+
+            //assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+     
+    }
+}
