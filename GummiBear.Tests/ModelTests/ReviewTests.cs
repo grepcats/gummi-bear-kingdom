49fc402101abf0eb1f56501eb9d0eb6bfe7cd2fc
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-27 11:05:10 -0700

    add Review model test for setters

--- GummiBear.Tests/ModelTests/ReviewTests.cs
+++ GummiBear.Tests/ModelTests/ReviewTests.cs
@@ -11,7 +11,7 @@ namespace GummiBear.Tests.ModelTests
         public void GetReviewProperties_ReturnCorrectData_All()
         {
             //arrange
-            Review newReview = new Review { ReviewId = 1, Author = "Bob", ContentBody = "This product is great", Rating = 2 };
+            Review newReview = new Review { ReviewId = 1, Author = "Bob", ContentBody = "This product is great", Rating = 5 };
 
             //act
             string author = newReview.Author;
@@ -21,7 +21,24 @@ namespace GummiBear.Tests.ModelTests
             //assert
             Assert.AreEqual(author, "Bob");
             Assert.AreEqual(body, "This product is great");
-            Assert.AreEqual(rating, 2);
+            Assert.AreEqual(rating, 5);
+        }
+
+        [TestMethod]
+        public void SetReviewProperties_SetThem_Void()
+        {
+            //arrange
+            Review newReview = new Review { ReviewId = 1, Author = "Bob", ContentBody = "This product is great", Rating = 5 };
+
+            //act
+            newReview.Author = "Frank";
+            newReview.ContentBody = "This product is terrible";
+            newReview.Rating = 1;
+
+            //assert
+            Assert.AreEqual(newReview.Author, "Frank");
+            Assert.AreEqual(newReview.ContentBody, "This product is terrible");
+            Assert.AreEqual(newReview.Rating, 1);
         }
     }
 }
