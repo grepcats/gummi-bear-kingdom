596950419d818c0a6b5b8c76f46d1aa4f5547e04
Kayla Ondracek <kayla.renee@gmail.com>
2018-04-27 10:05:07 -0700

    add moq and set up mock database. add first controller test for productcontroller for index view return

--- GummiBear.Tests/ControllerTests/ProductsControllerTests.cs
+++ GummiBear.Tests/ControllerTests/ProductsControllerTests.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GummiBear.Controllers;
-//using GummiBear.Models.Repositories;
+using GummiBear.Models.Repositories;
 using GummiBear.Models;
 using Microsoft.AspNetCore.Mvc;
-//using Moq;
+using Moq;
 using System.Linq;
 
 namespace GummiBear.Tests.ControllerTests
 {
-    class ProductsControllerTests
+    [TestClass]
+    public class ProductsControllerTests
     {
+        Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+        private void DbSetup()
+        {
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product("Code Complete by Steve McConnell", "Widely considered one of the best practical guides to programming...", (decimal)29.97),
+                new Product("Clean Code by Robert C Martin", "Even bad code can function...", (decimal)34.00),
+                new Product("Cracking the Coding Interview by Gayle McDowell", "Cracking the Coding Interview, 6th Edition is here to help you through this process...", (decimal)37.95)
+            }.AsQueryable());
+        }
+
+        [TestMethod]
+        public void Mock_GetViewResultIndex_ActionResult()
+        {
+            //arrange
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+
+            //act
+            var result = controller.Index();
+
+            //assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult));
+        }
     }
 }
